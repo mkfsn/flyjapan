7c47f62d4bbf8dd8c1f2435b9bbc0e3eb52de6c8
Pei-Ming Wu <p408865@gmail.com>
2019-03-16 16:40:08 +0800

    fix: fetch since the nearest friday

--- examples/weekend.go
+++ examples/weekend.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"sort"
 	"time"
 
 	"github.com/mkfsn/flyjapan"
@@ -15,68 +16,57 @@ func main() {
 	if err != nil {
 		log.Fatalln("error:", err)
 	}
+	for _, place := range []string{"HND", "KIX", "OKA"} {
+		fetchFlightToCity(peach, place)
+	}
+}
 
-	ctx := context.Background()
-
-	week := time.Hour * 24 * 7
-	places := []string{"HND", "KIX", "OKA"}
-	for _, place := range places {
-		for i := 0; i < 23; i++ {
-
-			friday := time.Date(2019, 3, 22, 0, 0, 0, 0, time.Local).Add(week * time.Duration(i))
-			saturday := time.Date(2019, 3, 23, 0, 0, 0, 0, time.Local).Add(week * time.Duration(i))
-			sunday := time.Date(2019, 3, 24, 0, 0, 0, 0, time.Local).Add(week * time.Duration(i))
-			monday := time.Date(2019, 3, 25, 0, 0, 0, 0, time.Local).Add(week * time.Duration(i))
-
-			x, err := fetch(peach, ctx, "TPE", place, friday, sunday)
-			if err != nil {
-				break
-			}
-			y, err := fetch(peach, ctx, "TPE", place, saturday, monday)
-			if err != nil {
-				break
-			}
-
-			var i, j flyjapan.Flight
-
-			i1 := x.InBound().Cheapest()
-			i2 := y.InBound().Cheapest()
-			if a, b := i1.CheapestFare(), i2.CheapestFare(); a.BaseFare > b.BaseFare {
-				i = i1
-			} else {
-				i = i2
-			}
-
-			j1 := x.OutBound().Cheapest()
-			j2 := y.OutBound().Cheapest()
-			if a, b := j1.CheapestFare(), j2.CheapestFare(); a.BaseFare > b.BaseFare {
-				j = j1
-			} else {
-				j = j2
-			}
-
-			f1, f2 := i.CheapestFare().BaseFare, j.CheapestFare().BaseFare
-			if f1 != 0 && f2 != 0 && f1+f2 < 5000 {
+func fetchFlightToCity(searcher flyjapan.Searcher, city string) {
+	today := time.Now().Truncate(time.Hour * 24)
+	nextFriday := today.Add((time.Duration(12-int(today.Weekday())%7) * 24 * time.Hour))
+	for i := 0; i < 23; i, nextFriday = i+1, nextFriday.Add(time.Hour*24*7) {
+		inBound, outBound := fetchWeekendFlightToCity(searcher, city, nextFriday)
+		inBound = inBound.FilterBy(flyjapan.AvailableFlight)
+		outBound = outBound.FilterBy(flyjapan.AvailableFlight)
+		sort.Sort(flyjapan.SortByBaseFare(inBound))
+		sort.Sort(flyjapan.SortByBaseFare(outBound))
+		for i := 0; i < len(inBound) && i < 2; i++ {
+			for j := 0; j < len(outBound) && j < 2; j++ {
+				x, y := inBound[i], outBound[j]
+				a, b := x.Cheapest(), y.Cheapest()
+				if a+b > 3500 {
+					continue
+				}
+				dateFormat := "2006/01/02 15:04:05"
 				fmt.Printf("[%s(%v)->%s(%v)]%5v --> [%s(%v)->%s(%v)]%5v = %5v\n",
-					i.OriginCode,
-					i.DepartureTime,
-					i.DestinationCode,
-					i.ArrivalTime,
-					f1,
-					j.OriginCode,
-					j.DepartureTime,
-					j.DestinationCode,
-					j.ArrivalTime,
-					f2,
-					f1+f2,
+					x.Origin.Code, x.DepartureTime.Format(dateFormat), x.Destination.Code, x.ArrivalTime.Format(dateFormat), a,
+					y.Origin.Code, y.DepartureTime.Format(dateFormat), y.Destination.Code, y.ArrivalTime.Format(dateFormat), b,
+					a+b,
 				)
 			}
 		}
 	}
 }
 
-func fetch(peach flyjapan.Searcher, ctx context.Context, from, to string, begin, end time.Time) (flyjapan.Result, error) {
-	return peach.Search(ctx, flyjapan.Query{
+func fetchWeekendFlightToCity(searcher flyjapan.Searcher, city string, friday time.Time) (flyjapan.Flights, flyjapan.Flights) {
+	saturday := friday.Add(time.Hour * 24 * 1)
+	sunday := friday.Add(time.Hour * 24 * 2)
+	monday := friday.Add(time.Hour * 24 * 3)
+	x, err := fetch(searcher, "TPE", city, friday, sunday)
+	if err != nil {
+		return nil, nil
+	}
+	y, err := fetch(searcher, "TPE", city, saturday, monday)
+	if err != nil {
+		return nil, nil
+	}
+	inBound := append(x.InBound(), y.InBound()...)
+	outBound := append(x.OutBound(), y.OutBound()...)
+	return inBound, outBound
+}
+
+func fetch(peach flyjapan.Searcher, from, to string, begin, end time.Time) (flyjapan.Result, error) {
+	return peach.Search(context.Background(), flyjapan.Query{
 		DepartureDate:        begin,
 		ReturnDate:           end,
 		DepartureAirportCode: from,
