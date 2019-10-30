819e14dcf50075296360277995df2c77fb3be975
Pei-Ming Wu <p408865@gmail.com>
2019-10-30 21:10:20 +0800

    fix: update example

--- examples/weekend.go
+++ examples/weekend.go
@@ -2,95 +2,87 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"log"
 	"sort"
 	"time"
 
-	"github.com/mkfsn/flyjapan"
-	"github.com/mkfsn/flyjapan/peach"
-
 	. "github.com/logrusorgru/aurora"
+	"github.com/mkfsn/flyjapan"
+	"github.com/mkfsn/flyjapan/airlines"
 )
 
 func main() {
-	peach, err := peach.New()
-	if err != nil {
-		log.Fatalln("error:", err)
+	flag.Parse()
+	args := flag.Args()
+	if len(args) == 0 {
+		args = []string{"FUK", "HND", "KIX", "OKA"}
 	}
-	for _, place := range []string{"HND", "KIX", "OKA"} {
-		fetchFlightToCity(peach, place)
+	for _, city := range args {
+		fetchByDestination(city)
 	}
 }
 
-func fetchFlightToCity(searcher flyjapan.Searcher, city string) {
+func fetchByDestination(city string) {
+	// Query(From(date), To(date), Airport(), Airport(), SortBy(fn), Airline(), Airline(), RepeatWeeks(n), FilterBy(fn))
 	today := time.Now().Truncate(time.Hour * 24)
-	nextFriday := today.Add((time.Duration(12-int(today.Weekday())%7) * 24 * time.Hour))
-	for i := 0; i < 23; i, nextFriday = i+1, nextFriday.Add(time.Hour*24*7) {
-		inBound, outBound := fetchWeekendFlightToCity(searcher, city, nextFriday)
-		inBound = inBound.FilterBy(flyjapan.AvailableFlight)
-		outBound = outBound.FilterBy(flyjapan.AvailableFlight)
-		sort.Sort(flyjapan.SortByBaseFare(inBound))
-		sort.Sort(flyjapan.SortByBaseFare(outBound))
-		for i := 0; i < len(inBound) && i < 2; i++ {
-			for j := 0; j < len(outBound) && j < 2; j++ {
-				x, y := inBound[i], outBound[j]
-				a, b := x.Cheapest(), y.Cheapest()
-				if a+b > 3500 {
-					continue
-				}
-				dateFormat := "2006/01/02 15:04:05"
-				fmt.Printf("%s%v%s%s%v%5v",
-					BgBlue(Black(fmt.Sprintf("[%s]", x.Origin.Code))),
-					BgMagenta(Black(x.DepartureTime.Format(dateFormat))),
-					BgGray(Black("->")),
-					BgBlue(Black(fmt.Sprintf("[%s]", x.Destination.Code))),
-					BgMagenta(Black(x.ArrivalTime.Format(dateFormat))),
-					BgBrown(Black(a)),
-				)
-				fmt.Printf(" + ")
-				fmt.Printf("%s%v%s%s%v%5v",
-					BgBlue(Black(fmt.Sprintf("[%s]", y.Origin.Code))),
-					BgMagenta(Black(y.DepartureTime.Format(dateFormat))),
-					BgGray(Black("->")),
-					BgBlue(Black(fmt.Sprintf("[%s]", y.Destination.Code))),
-					BgMagenta(Black(y.ArrivalTime.Format(dateFormat))),
-					BgBrown(Black(b)),
-				)
-				if a+b < 3000 {
-					fmt.Printf(" = %5v\n", BgGreen(Red(a+b)))
-				} else {
-					fmt.Printf(" = %5v\n", BgGreen(Black(a+b)))
-				}
-			}
+	friday := today.Add(time.Duration(12-int(today.Weekday())%7) * 24 * time.Hour)
+	for i := 0; i < 28; i, friday = i+1, friday.Add(time.Hour*24*7) {
+		saturday, sunday, monday := friday.Add(time.Hour*24), friday.Add(time.Hour*24*2), friday.Add(time.Hour*24*3)
+		ch, err := flyjapan.Query(
+			context.Background(),
+			flyjapan.Airline(airlines.AirlinePeach),
+			flyjapan.DestinationAirport(city),
+			flyjapan.SourceAirport("TPE"),
+			flyjapan.ToAndFrom(friday, sunday),
+			flyjapan.ToAndFrom(friday, monday),
+			flyjapan.ToAndFrom(saturday, monday),
+		)
+		if err != nil {
+			log.Fatalln("error:", err)
+		}
+		for res := range ch {
+			handleResult(res)
 		}
 	}
 }
 
-func fetchWeekendFlightToCity(searcher flyjapan.Searcher, city string, friday time.Time) (flyjapan.Flights, flyjapan.Flights) {
-	saturday := friday.Add(time.Hour * 24 * 1)
-	sunday := friday.Add(time.Hour * 24 * 2)
-	monday := friday.Add(time.Hour * 24 * 3)
-	x, err := fetch(searcher, "TPE", city, friday, sunday)
-	if err != nil {
-		return nil, nil
+func handleResult(res flyjapan.Result) {
+	inBound := res.Inbound.FilterBy(airlines.AvailableFlight)
+	outBound := res.Outbound.FilterBy(airlines.AvailableFlight)
+	sort.Sort(airlines.SortByBaseFare(inBound))
+	sort.Sort(airlines.SortByBaseFare(outBound))
+	for i := 0; i < len(inBound) && i < 3; i++ {
+		for j := 0; j < len(outBound) && j < 3; j++ {
+			x, y := inBound[i], outBound[j]
+			a, b := x.Cheapest(), y.Cheapest()
+			if a+b > 4500 {
+				continue
+			}
+			dateFormat := "2006/01/02 15:04:05"
+			fmt.Printf("%s%v%s%s%v%5v",
+				BgBlue(Black(fmt.Sprintf("[%s]", x.Origin.Code))),
+				BgMagenta(Black(x.DepartureTime.Format(dateFormat))),
+				BgGray(Black("->")),
+				BgBlue(Black(fmt.Sprintf("[%s]", x.Destination.Code))),
+				BgMagenta(Black(x.ArrivalTime.Format(dateFormat))),
+				BgBrown(Black(a)),
+			)
+			fmt.Printf(" + ")
+			fmt.Printf("%s%v%s%s%v%5v",
+				BgBlue(Black(fmt.Sprintf("[%s]", y.Origin.Code))),
+				BgMagenta(Black(y.DepartureTime.Format(dateFormat))),
+				BgGray(Black("->")),
+				BgBlue(Black(fmt.Sprintf("[%s]", y.Destination.Code))),
+				BgMagenta(Black(y.ArrivalTime.Format(dateFormat))),
+				BgBrown(Black(b)),
+			)
+			if a+b < 3000 {
+				fmt.Printf(" = %5v\n", BgGreen(Red(a+b)))
+			} else {
+				fmt.Printf(" = %5v\n", BgGreen(Black(a+b)))
+			}
+		}
 	}
-	y, err := fetch(searcher, "TPE", city, saturday, monday)
-	if err != nil {
-		return nil, nil
-	}
-	inBound := append(x.InBound(), y.InBound()...)
-	outBound := append(x.OutBound(), y.OutBound()...)
-	return inBound, outBound
-}
-
-func fetch(peach flyjapan.Searcher, from, to string, begin, end time.Time) (flyjapan.Result, error) {
-	return peach.Search(context.Background(), flyjapan.Query{
-		DepartureDate:        begin,
-		ReturnDate:           end,
-		DepartureAirportCode: from,
-		ArrivalAirportCode:   to,
-		IsReturn:             true,
-		AdultCount:           1,
-	})
 }
