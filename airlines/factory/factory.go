0eb4786e37ed3c639e999d31141a6805ab5faefa
Pei-Ming Wu <p408865@gmail.com>
2019-12-28 17:43:39 +0800

    fix: support tiger airline

--- airlines/factory/factory.go
+++ airlines/factory/factory.go
@@ -1,14 +1,20 @@
 package factory
 
 import (
+	"strings"
+
 	"github.com/mkfsn/flyjapan/airlines"
 	"github.com/mkfsn/flyjapan/airlines/peach"
+	"github.com/mkfsn/flyjapan/airlines/tiger"
 )
 
-func CreateAirline(airline airlines.AirlineName) (airlines.Airline, error) {
+func CreateAirline(name airlines.AirlineName) (airlines.Airline, error) {
+	airline := airlines.AirlineName(strings.ToLower(string(name)))
 	switch airline {
 	case airlines.AirlinePeach:
 		return peach.New()
+	case airlines.AirlineTiger:
+		return tiger.New()
 	default:
 	}
 	return nil, ErrUnsupportedAirline
